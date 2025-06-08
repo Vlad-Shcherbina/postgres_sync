86333b5c40e1ce40f97fb8f63a2fd32020ff4d45
Vlad-Shcherbina <vlad.shcherbina@gmail.com>
2025-06-08 23:00:22 +0200

    Transaction

--- postgres_sync/src/lib.rs
+++ postgres_sync/src/lib.rs
@@ -16,7 +16,10 @@ use postgres_types::{IsNull, Type};
 pub use fallible_iterator;
 pub use postgres_types::{BorrowToSql, FromSql, ToSql};
 
+pub use crate::transaction::Transaction;
+
 mod conn_str;
+mod transaction;
 
 pub type Error = Box<dyn StdError + Send + Sync>;
 
@@ -453,65 +456,3 @@ impl FallibleIterator for RowIter {
     }
 }
 
-pub struct Transaction<'a> {
-    client: &'a mut Client,
-    finished: bool,
-}
-
-impl Client {
-    pub fn transaction(&mut self) -> Result<Transaction<'_>, Error> {
-        self.batch_execute("BEGIN")?;
-        Ok(Transaction {
-            client: self,
-            finished: false,
-        })
-    }
-}
-
-impl<'a> Transaction<'a> {
-    pub fn commit(mut self) -> Result<(), Error> {
-        if !self.finished {
-            self.client.batch_execute("COMMIT")?;
-            self.finished = true;
-        }
-        Ok(())
-    }
-
-    pub fn rollback(mut self) -> Result<(), Error> {
-        if !self.finished {
-            self.client.batch_execute("ROLLBACK")?;
-            self.finished = true;
-        }
-        Ok(())
-    }
-
-    pub fn batch_execute(&mut self, query: &str) -> Result<(), Error> {
-        self.client.batch_execute(query)
-    }
-
-    pub fn execute(&mut self, query: &str, params: &[&(dyn ToSql + Sync)]) -> Result<u64, Error> {
-        self.client.execute(query, params)
-    }
-
-    pub fn query_one(&mut self, query: &str, params: &[&(dyn ToSql + Sync)]) -> Result<Row, Error> {
-        self.client.query_one(query, params)
-    }
-
-    pub fn query_raw<P, I>(&mut self, query: &str, params: I) -> Result<RowIter, Error>
-    where
-        P: BorrowToSql,
-        I: IntoIterator<Item = P>,
-        I::IntoIter: ExactSizeIterator,
-    {
-        self.client.query_raw(query, params)
-    }
-}
-
-impl Drop for Transaction<'_> {
-    fn drop(&mut self) {
-        if !self.finished {
-            let _ = self.client.batch_execute("ROLLBACK");
-            self.finished = true;
-        }
-    }
-}
